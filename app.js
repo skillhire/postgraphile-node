5f765696d02928c777bda279d65176d995aa5a45
Rami Bitar <rbitar@gmail.com>
2021-10-06 18:58:41 -0400

    Postgraphile server

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 const dotenv = require("dotenv")
 dotenv.config()
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(postgraphile)
 
 // catch 404 and forward to error handler
